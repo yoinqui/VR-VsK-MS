26a0938854e3ca45c2af4fdaaebe23ce8fc28151
yoinqui <yoann.inquimbert@viacesi.fr>
2021-01-18 15:57:29 +0100

    JsonLevelLoader 3.0

--- Assets/VR-Vs-KMS/Scripts/LoadDataScene.cs
+++ Assets/VR-Vs-KMS/Scripts/LoadDataScene.cs
@@ -8,10 +8,6 @@ public class LoadDataScene : MonoBehaviour
             && JSONLevel.Inst.ThrowablesCount != 0)
         {
             Debug.LogError("JSON Level Data Management --> Done");
-
-            Debug.LogError("SpawnCount: " + JSONLevel.Inst.SpawnCount);
-            Debug.LogError("ContaminationAreaCount: " + JSONLevel.Inst.ContaminationAreaCount);
-            Debug.LogError("ThrowablesCount: " + JSONLevel.Inst.ThrowablesCount);
         } else
         {
             Debug.LogError("JSON Level_RA Import --> Load Error");
