5bc56a0e669d5ca9ba7d3de7fac1da333d94621e
EmmanuelBousser <emmanuel.bousser@viacesi.fr>
2021-01-19 19:42:25 +0100

    life bare

---
+++ Assets/VR-Vs-KMS/Scripts/LifeManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LifeManager : MonoBehaviour
+{
+    public float baseHealthPoints = 10.0f;
+    private float healthPoints;
+    public GameObject lifeBar;
+
+    public delegate void OnDeath(GameObject controller);
+
+    /// <summary>
+    /// The Death event.
+    /// Use like this :
+    /// "LifeManager.onDeath += this.myMethode;"
+    /// </summary>
+    public static event OnDeath onDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPoints = baseHealthPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthPoints <= 0)
+        {
+            healthPoints = baseHealthPoints;
+            if (onDeath != null)
+            {
+                onDeath(this.gameObject);
+            }
+        }
+    }
+
+    public void ReduceHealth(float damage)
+    {
+        healthPoints -= damage;
+        Debug.Log(healthPoints);
+        LifeBarUpdate();
+    }
+
+    public void LifeBarUpdate()
+    {
+        lifeBar.GetComponent<Image>().fillAmount = healthPoints / 10;
+    }
+}
