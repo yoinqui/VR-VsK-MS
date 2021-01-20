ca9ecb7926b2e2b0415cba74949c45c9bd78e426
Florian <florian.pfeifer@viacesi.fr>
2021-01-20 10:05:44 +0100

    Add spawner management

--- Assets/VR-Vs-KMS/Scripts/LifeManager.cs
+++ Assets/VR-Vs-KMS/Scripts/LifeManager.cs
@@ -8,7 +8,7 @@ public class LifeManager : MonoBehaviour
     private float healthPoints;
     public GameObject lifeBar;
 
-    public delegate void OnDeath(GameObject controller);
+    public delegate void OnDeath(GameObject player);
 
     /// <summary>
     /// The Death event.
@@ -28,11 +28,11 @@ public class LifeManager : MonoBehaviour
     {
         if (healthPoints <= 0)
         {
-            healthPoints = baseHealthPoints;
             if (onDeath != null)
             {
                 onDeath(this.gameObject);
             }
+            healthPoints = baseHealthPoints;
         }
     }
 
